f3979d37c165122fb3327c39860e9d6e9314a8bd
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-05-19 00:24:45 -0300

    lorad: add circular queue insert() and delete()

--- src/lorad/lora_circular_queue.c
+++ src/lorad/lora_circular_queue.c
@@ -6,6 +6,8 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
+
+#include <string.h>
 #include "lora_circular_queue.h"
 
 /* Initializes the sentinels must be called at the begining of the code */
@@ -93,12 +95,189 @@ int lorad_close_queue(struct lorad_queue *queue){
 	return 0;
 }
 
+static struct *lorad_data_node get_queue_node_subset (struct lorad_queue *queue) {
+
+	switch (queue->type){
+	
+	case DATA_TX:
+		return queue->node_set->lorad_data_tx_node_set
+
+	case DATA_RX:
+		return queue->node_set->lorad_data_rx_node_set
+
+	case MGM_TX:
+		return queue->node_set->lorad_mgm_tx_node_set
+
+	case MGM_RX:
+		return queue->node_set->lorad_mgm_rx_node_set
+		
+	default:
+		return NULL;
+	}
+}
+
+static struct *lorad_queue get_queue_sentinel (struct lorad_queue *queue) {
+
+	switch (queue->type){
+	
+	case DATA_TX:
+		return &(queue->node_set->sentinel_data_tx);
+
+	case DATA_RX:
+		return &(queue->node_set->sentinel_data_rx);
+
+	case MGM_TX:
+		return &(queue->node_set->sentinel_mgm_tx);
+
+	case MGM_RX:
+		return &(queue->node_set->sentinel_mgm_rx);
+		
+	default:
+		return NULL;
+	}
+}
+
+static size_t get_queue_data_len (struct lorad_queue *queue) {
+
+	switch (queue->type){
+	
+	case DATA_TX:
+		return LORAD_DATA_MAX_PKT_LEN;
+
+	case DATA_RX:
+		return LORAD_DATA_MAX_PKT_LEN;
+
+	case MGM_TX:
+		return LORAD_MGM_MAX_PKT_LEN;
+
+	case MGM_RX:
+		return LORAD_MGM_MAX_PKT_LEN;
+		
+	default:
+		return 0;
+	}
+}
+
+static int switch_head_node_queue (struct lorad_queue *dst_queue,
+	struct lorad_queue *src_queue){
+
+	struct lorad_node_set *node_subset;
+	lorad_node_fd node_to_switch;
+
+	if (src_queue->type != dst_queue->type)
+		return -1;
+	if (src_queue->node_set != dst_queue->node_set)
+		return -1;
+	if (src_queue->tail == LORAD_EMPTY_QUEUE_FD)
+		return -1;
+
+	node_subset = get_queue_node_subset(src_queue);
+	if (node_subset == NULL)
+		return -1;
+	/* Get the source queue's head */
+	node_to_switch = node_subset[src_queue->tail].next;
+	
+
+	/* Removing the node from the source queue head */
+
+	/* One node remaining case */
+	if (src_queue->tail == node_to_switch) {
+		/* Make the queue's tail points to nothing */
+		src_queue->tail = LORAD_EMPTY_QUEUE_FD;
+	} else {
+		/* Make the queue's tail points to the new head */
+		src_queue->tail = node_subset[node_to_switch].next;
+	}
+
+
+	/* Adding the node in the destiny queue tail */
+
+	/* Make the new node point to the head */
+	node_subset[node_to_switch].next = node_subset[dst_queue->tail].next;
+	/* Make old tail node point to the new node */
+	node_subset[dst_queue->tail].next = node_to_switch;
+	/* Make new node the queue's tail */
+	dst_queue->tail = node_to_switch;
+
+	return 0;
+}
+
+static int head_update(struct lorad_queue *queue, uint8_t* data){
+
+	struct lorad_node_set *node_subset;
+	lorad_node_fd head_node;
+	size_t max_len = get_queue_data_len(queue);
+
+	if (len == 0)
+		return -1;
+
+	node_subset = get_queue_node_subset(queue);
+	if (node_subset == NULL)
+		return -1;
+
+	/* Get the queue's head */
+	head_node = node_subset[queue->tail].next;
+
+	/* Update head data */
+	memcpy(node_subset[head_node].data, data, max_len);
+
+	return 0;
+}
+
 int lorad_queue_insert(struct lorad_queue *queue, uint8_t* data){
+	
+	int err = 0;
+	struct lorad_queue *sentinel;
+
+	/* Check if queue was properely initialized */
+	if(queue->node_set == NULL)
+		return -1;
+
+	/* 
+	 * Get the sentinel that manages the proper node
+	 * subset for insertion in the queue's node set.
+	 */
+	sentinel = get_queue_sentinel(queue);
+	if (sentinel == NULL)
+		return -1;
+
+	/* Get an avaliable node from node set through the sentinel */
+	err = switch_head_node_queue (queue, sentinel);
+	if (err < 0)
+		return -1;
+
+	err = head_update(queue, data);
+	if (err < 0)
+		return -1;
+
 	return 0;
 }
+
 int lorad_queue_delete(struct lorad_queue *queue){
+	
+	int err = 0;
+	struct lorad_queue *sentinel;
+
+	/* Check if queue was properely initialized */
+	if(queue->node_set == NULL)
+		return -1;
+
+	/* 
+	 * Get the sentinel that manages the proper node
+	 * subset for deletion in the queue's node set.
+	 */
+	sentinel = get_queue_sentinel(queue);
+	if (sentinel == NULL)
+		return -1;
+
+	/* Make the node avaliable in the node set through the sentinel */
+	err = switch_head_node_queue (sentinel, queue);
+	if (err < 0)
+		return -1;
+
 	return 0;
 }
+
 int lorad_queue_peek(struct lorad_queue *queue, uint8_t* peek_buffer){
 	return 0;
 }
