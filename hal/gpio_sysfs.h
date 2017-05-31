396cb3611ea655aa19c1c257d5dd8acc6484cd24
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-05-31 15:32:13 -0300

    hal: Update gpio sysfs header file

--- hal/gpio_sysfs.h
+++ hal/gpio_sysfs.h
@@ -6,3 +6,32 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
+
+
+#ifndef __HAL_GPIO_H__
+#define __HAL_GPIO_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define INPUT 0
+#define OUTPUT 1
+
+#define LOW 0
+#define HIGH 1
+
+int hal_gpio_setup(void);
+void hal_gpio_unmap(void);
+void hal_gpio_pin_mode(uint8_t, uint8_t);
+void hal_gpio_digital_write(uint8_t, uint8_t);
+int hal_gpio_digital_read(uint8_t);
+int hal_gpio_analog_read(uint8_t);
+void hal_gpio_analog_reference(uint8_t mode);
+void hal_gpio_analog_write(uint8_t, int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/hal/gpio/gpio_sysfs.c
+++ src/hal/gpio/gpio_sysfs.c
@@ -6,3 +6,45 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
+
+#include "hal/gpio_sysfs.h"
+
+int hal_gpio_setup(void)
+{
+	return 0;
+}
+
+void hal_gpio_unmap(void)
+{
+
+}
+
+void hal_gpio_pin_mode(uint8_t, uint8_t)
+{
+
+}
+
+void hal_gpio_digital_write(uint8_t, uint8_t)
+{
+
+}
+
+int hal_gpio_digital_read(uint8_t)
+{
+	return 0;
+}
+
+int hal_gpio_analog_read(uint8_t)
+{
+	return 0;
+}
+
+void hal_gpio_analog_reference(uint8_t mode)
+{
+
+}
+
+void hal_gpio_analog_write(uint8_t, int)
+{
+
+}
