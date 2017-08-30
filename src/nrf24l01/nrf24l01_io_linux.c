8d5adfec11f71e4ae6b9c18f2e59590f900c4e14
Guilherme Sales Santa Cruz <gssc@cin.ufpe.br>
2017-08-30 16:45:44 -0300

    TESTE 2 - remove the (appearently) unecessary

--- src/nrf24l01/nrf24l01_io_linux.c
+++ src/nrf24l01/nrf24l01_io_linux.c
@@ -20,46 +20,46 @@
 #define CE	25
 #define IRQ	24
 
-#define LOW	0
-#define HIGH	1
+// #define LOW	0
+// #define HIGH	1
 
-#define BCM2709_RPI2		0x3F000000
-#define BCM2708_RPI		0x20000000
+// #define BCM2709_RPI2		0x3F000000
+// #define BCM2708_RPI		0x20000000
 
-#ifdef RPI2_BOARD
-#define BCM2708_PERI_BASE	BCM2709_RPI2
-#elif RPI_BOARD
-#define BCM2708_PERI_BASE	BCM2708_RPI
-#else
-#error Board identifier required to BCM2708_PERI_BASE.
-#endif
+// #ifdef RPI2_BOARD
+// #define BCM2708_PERI_BASE	BCM2709_RPI2
+// #elif RPI_BOARD
+// #define BCM2708_PERI_BASE	BCM2708_RPI
+// #else
+// #error Board identifier required to BCM2708_PERI_BASE.
+// #endif
 
-#define GPIO_BASE		(BCM2708_PERI_BASE + 0x200000)
-#define PAGE_SIZE		(4*1024)
-#define BLOCK_SIZE		(4*1024)
+// #define GPIO_BASE		(BCM2708_PERI_BASE + 0x200000)
+// #define PAGE_SIZE		(4*1024)
+// #define BLOCK_SIZE		(4*1024)
 
-/* Raspberry pi GPIO Macros */
-#define INP_GPIO(g)		(*(gpio+((g)/10)) &= ~(7<<(((g)%10)*3)))
-#define OUT_GPIO(g)		(*(gpio+((g)/10)) |=  (1<<(((g)%10)*3)))
-#define SET_GPIO_ALT(g, a)	(*(gpio+(((g)/10))) |= (((a) <= 3?(a)+4:(a) == 4?3:2)\
-							<< (((g)%10)*3)))
+// /* Raspberry pi GPIO Macros */
+// #define INP_GPIO(g)		(*(gpio+((g)/10)) &= ~(7<<(((g)%10)*3)))
+// #define OUT_GPIO(g)		(*(gpio+((g)/10)) |=  (1<<(((g)%10)*3)))
+// #define SET_GPIO_ALT(g, a)	(*(gpio+(((g)/10))) |= (((a) <= 3?(a)+4:(a) == 4?3:2)\
+// 							<< (((g)%10)*3)))
 
-/* sets bits which are 1 ignores bits which are 0 */
-#define GPIO_SET		*(gpio+7)
+// /* sets bits which are 1 ignores bits which are 0 */
+// #define GPIO_SET		*(gpio+7)
 
-/* clears bits which are 1 ignores bits which are 0 */
-#define GPIO_CLR		*(gpio+10)
+// /* clears bits which are 1 ignores bits which are 0 */
+// #define GPIO_CLR		*(gpio+10)
 
-/* 0 if LOW, (1<<g) if HIGH */
-#define GET_GPIO(g)		(*(gpio+13) & (1 << g))
+// /* 0 if LOW, (1<<g) if HIGH */
+// #define GET_GPIO(g)		(*(gpio+13) & (1 << g))
 
-/*
- * Pull up/pull down followed by pull up/down clock
- */
-#define GPIO_PULL		*(gpio+37)
-#define GPIO_PULLCLK0		*(gpio+38)
+// /*
+//  * Pull up/pull down followed by pull up/down clock
+//  */
+// #define GPIO_PULL		*(gpio+37)
+// #define GPIO_PULLCLK0		*(gpio+38)
 
-static volatile unsigned	*gpio;
+// static volatile unsigned	*gpio;
 
 void delay_us(float us)
 {
